c5983082219f101112e1729f2ef4719248c2dc50
Saksham Gupta <83531898+sakshamgupta298@users.noreply.github.com>
2022-04-17 21:41:26 +0530

    Create sprite.fs

---
+++ assets/shaders/sprite.fs
@@ -0,0 +1,11 @@
+#version 330 core
+
+in vec2 TexCoords;
+
+uniform sampler2D image;
+uniform vec3 spriteColor;
+
+void main()
+{
+	gl_FragColor = vec4(spriteColor , 1.0) * texture2D(image , TexCoords);
+}
